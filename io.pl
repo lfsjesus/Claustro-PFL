1119a80bed87dc19a470148ba98c030bbc380f0f
Luís Jesus <luisjesus__2003@outlook.com>
2023-10-30 10:52:27 +0000

    Starting doing menus. Choices working. Instructions draft done

--- io.pl
+++ io.pl
@@ -1,11 +1,15 @@
+:- use_module(library(lists)).
+:- use_module(library(between)).
+
 % Clears the terminal screen
 clear :- write('\e[2J').
 
 /****MENU IO****/
-% Documentation of format/2: https://sicstus.sics.se/sicstus/docs/latest4/html/sicstus.html/mpg_002dref_002dformat.html
-
 menuH1(Title) :-
     format('~n~`*t ~p ~`*t~75|~n', [Title]).
+
+menuString(String) :-
+    format('*~t~p~t*~75|~n', [String]).    
 
 menuHeaders(Options, Descriptions) :-
     format('*~t~p~t~37+~t~p~t~37+~t*~75|~n', [Options, Descriptions]).
@@ -19,3 +23,26 @@ menuChoice(Option, Description) :-
 
 menuFill :-
     format('~`*t~75|~n', []).
+
+
+readNumber(X) :- readNumber(X, 0).
+
+readNumber(X, X) :-
+    peek_code(10), !,
+    skip_line.
+
+readNumber(X, Acc) :-
+    get_code(C),
+    C >= 48, C =< 57,
+    NewAcc is Acc * 10 + C - 48,
+    readNumber(X, NewAcc).
+
+
+readInputBetween(Min, Max, Input) :-
+    format('Select an option [~d-~d]: ', [Min, Max]),
+    readNumber(Input),
+    between(Min, Max, Input), !.
+
+readInputBetween(Min, Max, Input) :-
+    format('Invalid input. Please select an option between ~d and ~d.~n', [Min, Max]),
+    readInputBetween(Min, Max, Input).
--- menu/menu.pl
+++ menu/menu.pl
@@ -1,4 +1,7 @@
+:- consult('menu_display.pl').
+
 mainMenu :-
+    repeat,
     clear,
     menuH1('CLAUSTRO'),
     menunl,
@@ -9,13 +12,54 @@ mainMenu :-
     menuChoice(3, 'Bot vs Bot'),
     menuChoice(4, 'Instructions'),
     menunl,
-    menuOption(0, 'Exit'),
+    menuChoice(0, 'Exit'),
     menunl,
-    menuFill, nl.
+    menuFill, nl,
+    
+    readInputBetween(0, 4, Num),
+    execMenuChoice(Num).
+
 
 /**
  *
  * Then process menu choices
  */
 
+execMenuChoice(0) :- quitGame.
+/*execMenuChoice(1) :- player vs player*/
+execMenuChoice(2) :- playerVsBot.
+execMenuChoice(3) :- botVsBot.
+execMenuChoice(4) :- instructions.
+
+
+
+/**
+* Menus
+*/
+quitGame :- 
+    clear, nl,
+    menuFill, 
+    menuH1('Thanks for playing!'), nl,
+    menuFill.
+
+/* playerVsPlayer :- */
+
+/* playerVsBot :- */
+
+/* botVsBot :- */
+
+
+/* chooseBotDifficulty(...) :- */
+
+
+instructions :-
+    clear,
+    menuH1('Instructions'),
+    menunl,
+    showInstructions,
+    menunl,
+    menuFill, nl,
+    write('Press any key to return to the main menu...'), nl,
+    skip_line,
+    fail.
 
