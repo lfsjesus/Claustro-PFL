9109b73c543e60f4ebb48ecda403031351c65cc7
Miguel Rocha <roocha99pt@gmail.com>
2023-11-02 17:26:51 +0000

    Added Can move

--- board/board.pl
+++ board/board.pl
@@ -93,3 +93,100 @@ getSquare(X, Y, Board, Value) :-
 checkSquareType(X, Y, Type, Board) :-
     getSquare(X, Y, Board, Type).
 
+piece(Color, X, Y).
+
+
+
+% Can Move From (X1,Y1) -> (X2,Y2)
+
+% Can Move From (X1,Y1) -> (X2,Y2) Blue - Move to the left and check if the square is empty
+canMove(blue, X1, Y1, X2, Y2, Board) :-
+    X2 =:= X1 - 1,
+    Y2 =:= Y1,
+    checkSquareType(X2, Y2, empty, Board).
+
+% Can Move From (X1,Y1) -> (X2,Y2) Blue - Move to the left and check if the square is blueGoal
+canMove(blue, X1, Y1, X2, Y2, Board) :-
+    X2 =:= X1 - 1,
+    Y2 =:= Y1,
+    checkSquareType(X2, Y2, blueGoal, Board).
+
+% Can Move From (X1,Y1) -> (X2,Y2) Blue - Move Down and check if the square is empty
+canMove(blue, X1, Y1, X2, Y2, Board) :-
+    X2 =:= X1,
+    Y2 =:= Y1 + 1,
+    checkSquareType(X2, Y2, empty, Board).
+
+% Can Move From (X1,Y1) -> (X2,Y2) Blue - Move Down and check if the square is blueGoal
+canMove(blue, X1, Y1, X2, Y2, Board) :-
+    X2 =:= X1,
+    Y2 =:= Y1 + 1,
+    checkSquareType(X2, Y2, blueGoal, Board).
+
+
+% Can Move From (X1,Y1) -> (X2,Y2) Green - Move to the right and check if the square is empty
+canMove(green, X1, Y1, X2, Y2, Board) :-
+    X2 =:= X1 + 1,
+    Y2 =:= Y1,
+    checkSquareType(X2, Y2, empty, Board).
+
+% Can Move From (X1,Y1) -> (X2,Y2) Green - Move to the right and check if the square is greenGoal
+canMove(green, X1, Y1, X2, Y2, Board) :-
+    X2 =:= X1 + 1,
+    Y2 =:= Y1,
+    checkSquareType(X2, Y2, greenGoal, Board).
+
+% Can Move From (X1,Y1) -> (X2,Y2) Green - Move Up and check if the square is empty
+canMove(green, X1, Y1, X2, Y2, Board) :-
+    X2 =:= X1,
+    Y2 =:= Y1 - 1,
+    checkSquareType(X2, Y2, empty, Board).
+
+% Can Move From (X1,Y1) -> (X2,Y2) Green - Move Up and check if the square is greenGoal
+canMove(green, X1, Y1, X2, Y2, Board) :-
+    X2 =:= X1,
+    Y2 =:= Y1 - 1,
+    checkSquareType(X2, Y2, greenGoal, Board).
+
+
+
+
+% Can Capture From (X1,Y1) -> (X2,Y2)
+
+% Can Capture From (X1,Y1) -> (X2,Y2) Blue - Move to the (X1 + 1, Y1 - 1) and check if the square is green
+
+
+
+/*
+
+%every possible move from a given position
+
+% Can Move Blue
+canMove(blue, X, Y, Board) :-
+    checkSquareType(X-1, Y, empty, Board).  % Move to the left and check if the square is empty
+
+canMove(blue, X, Y, Board) :-
+    checkSquareType(X-1, Y, blueGoal, Board).  % Move to the left and check if the square is blueGoal
+
+
+canMove(blue, X, Y, Board) :-
+    checkSquareType(X, Y+1, empty, Board).   % Move Down and check if the square is empty
+
+canMove(blue, X, Y, Board) :-
+    checkSquareType(X, Y+1, blueGoal, Board).  % Move Down and check if the square is blueGoal
+
+
+% Can Move Green
+canMove(green, X, Y, Board) :-
+    checkSquareType(X+1, Y, empty, Board).  % Move to the right and check if the square is empty
+
+canMove(green, X, Y, Board) :-
+    checkSquareType(X+1, Y, greenGoal, Board).  % Move to the right and check if the square is greenGoal
+
+canMove(green, X, Y, Board) :-
+    checkSquareType(X, Y-1, empty, Board).   % Move Up and check if the square is empty
+
+canMove(green, X, Y, Board) :-
+    checkSquareType(X, Y-1, greenGoal, Board).  % Move Up and check if the square is greenGoal
+
+*/
