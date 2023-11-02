3acb671c04d1ba64fba67524376c782f11cc1af7
Luís Jesus <luisjesus__2003@outlook.com>
2023-11-02 13:33:35 +0000

    starting game loop. overall intuition drafted

--- game/game.pl
+++ game/game.pl
@@ -3,15 +3,29 @@ gameInit(N, M, P1-P2) :-
     initial_state(N, M, (Turn, MoveHistory, Board)),
     setSpecialSquares(N, M, Board, NewBoard), 
     setInitialPieces(N, M, NewBoard, NewNewBoard),
-    printList(NewNewBoard).
-
-
-    /* display_game(GameState), */
-    
-
+    printList(NewNewBoard),
+    gameLoop((Turn, MoveHistory, Board), PlayerType, Difficulty, N, M).
 
 initial_state(N, M, (Turn, MoveHistory, Board)) :-
     setBoard(N, M, Board).
+
+
+gameLoop(GameState, PlayerType, Difficulty, N, M) :-
+    choosePiece(N, M, GameState, PlayerType, Piece).
+    %chooseMove(GameState, PlayerType, Piece, Move),
+    %move(GameState, Piece, Move, NewGameState),
+    %changeTurn(PlayerType, NewPlayerType, Difficulty),
+    %gameLoop(NewGameState, NewPlayerType, Difficulty), !.
+
+
+choosePiece(N, M, GameState, PlayerType, Piece) :-
+    nl,
+    write('CHOOSE A PIECE TO MOVE:'), nl, nl,
+    write('--- X coordinate: ---'), nl,
+    readInputBetween(1, N, X), nl, nl,
+    write('--- Y coordinate: ---'), nl,
+    readInputBetween(1, M, Y), nl, nl.
+    % something here.
 
 /*
 % initial_state(+N, +M, -GameState)
